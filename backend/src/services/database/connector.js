e40f370248c3d0cb38a183404d603e89024e9b2b
hikmatillo <dolgovdaniil007@gmail.com>
2023-03-05 03:15:25 +0700

    added auth connection with the backend

--- backend/src/services/database/connector.js
+++ backend/src/services/database/connector.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const connection = await mongoose.connect('mongodb://root:rootpassword@172.20.10.5:27017/');
+const connection = await mongoose.connect('mongodb://root:rootpassword@localhost:27017/');
 
 export default connection;
 
